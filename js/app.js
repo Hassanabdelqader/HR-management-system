a771c69365091ec891547cbcc7a0265de7393d31
Hassanabdelqader <terawihassan@gmail.com>
2022-07-07 13:27:52 +0300

    Optimize the Code

--- js/app.js
+++ js/app.js
@@ -14,26 +14,34 @@ emplyeeArr.push(this);
 
 }
 
-Employee.prototype.salary =function(){
+Employee.prototype.getSalary =function(){
 
     if (this.level.toUpperCase()=="SENIOR") {
-        this.salary = Math.floor(Math.random() * (2000 - 1500 + 1) ) + 1500;  
-        this.netSalary = Math.floor(this.salary - (this.salary * 0.075)); 
-        return this.netSalary; 
+        this.salary = Math.floor(Math.random() * (2000 - 1500 + 1) ) + 1500; 
+        return this.salary;
 
     }else if(this.level.toUpperCase()=="MID-SENIOR"){
         
         this.salary = Math.floor(Math.random() * (1500 - 1000 + 1) ) + 1000;
-        this.netSalary = Math.floor(this.salary - (this.salary * 0.075));
-        return this.netSalary;
+        return this.salary;
     
     }else if(this.level.toUpperCase()=="JUNIOR"){
-        this.salary = Math.floor(Math.random() * (1000 - 500 + 1) ) + 5000; 
-        this.netSalary = Math.floor(this.salary - (this.salary * 0.075));
-        return this.netSalary;
+        this.salary = Math.floor(Math.random() * (1000 - 500 + 1) ) + 500; 
+        return this.salary;
     }
     
 }
+
+Employee.prototype.getNetSalary =function(){
+
+        this.netSalary = Math.floor(this.getSalary() - (this.getSalary() * 0.075)); 
+        return this.netSalary;
+
+    }
+    
+
+
+
 
 const ghaziSamer  = new Employee(1000,"Ghazi Samer" , "Administration" , "Senior" );
 
@@ -50,13 +58,13 @@ const ranaSaleh = new Employee(1001,"Rana Saleh" , "Development" ,"Junior" );
 const hadiAhmad = new Employee(1001,"Hadi Ahmad" , "Finance" , "Mid-Senior" );
 
 Employee.prototype.showInfo=function(){
-    document.write(` Emplyee Name is : ${this.fullName},and his Salary is :  ${this.salary()} <br>`);
+    document.write(` Emplyee Name is : ${this.fullName},and his Salary is :  ${this.getSalary()} <br>`);
 }
 
 let table = ` <table border = 1> <tr><th>Name</th> <th>Salary</th> </tr> `;
 for (const key in emplyeeArr) {
 
-    table+=`<tr><td>${emplyeeArr[key].fullName}</td> <td>${emplyeeArr[key].salary()}</td></tr>`;
+    table+=`<tr><td>${emplyeeArr[key].fullName}</td> <td>${emplyeeArr[key].getSalary()}</td></tr>`;
   
    // emplyeeArr[key].showInfo();
 }
